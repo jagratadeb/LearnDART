4d7948eff1aed1411f4c52d9e9266f3d6be68390
Jagrata Deb <jagratadeb2004@gmail.com>
2025-06-27 00:26:13 +0530

    take user input

--- 01 Hello World/01helloWorld.dart
+++ 01 Hello World/01helloWorld.dart
@@ -9,6 +9,10 @@ void main() {
   stdout.write(
     "Dart is a modern, object-oriented programming language designed for building high-performance apps across mobile, web, desktop, and backend. It’s the backbone of Flutter, enabling fast development with features like hot reload, sound null safety, and native compilation. Dart’s clean syntax and powerful tooling make it popular for cross-platform development.",
   );
+
+  stdout.writeln("\nEnter your name: ");
+  String? name = stdin.readLineSync();
+  print("Welcome, $name!");
 }
 
 // Difference between print and stdout.write:
