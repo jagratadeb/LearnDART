79db89b34e7303ddb05ea2ec1ef9592a4e7e9cad
Jagrata Deb <jagratadeb2004@gmail.com>
2025-06-27 02:46:53 +0530

    add code for null safety

---
+++ 04 Data Types/08nullSafety.dart
@@ -0,0 +1,12 @@
+// Null Safety helps you avoid null exceptions in your code.
+// It ensures that variables cannot be null unless explicitly declared as nullable.
+
+void main() {
+  String? language = null; // Nullable variable
+  double? version = 3.8; // Nullable variable
+  print("Language: $language");
+  print("Version: $version");
+}
+
+
+// Null safety helps protect your app from unexpected crashes that occur when your code tries to use null values in operations.
