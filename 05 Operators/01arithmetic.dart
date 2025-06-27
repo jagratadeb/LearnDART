43267dcfcc720a0424d2ccdbd25c9154c4744ab9
Jagrata Deb <jagratadeb2004@gmail.com>
2025-06-27 18:45:02 +0530

    add code for arithmetic operators

---
+++ 05 Operators/01arithmetic.dart
@@ -0,0 +1,26 @@
+// Dart program to demonstrate arithmetic operators
+void main() {
+  int num1 = 10;
+  int num2 = 5;
+
+  // Addition
+  int sum = num1 + num2;
+
+  // Subtraction
+  int difference = num1 - num2;
+
+  // Multiplication
+  int product = num1 * num2;
+
+  // Division
+  double quotient = num1 / num2;
+
+  // Modulus
+  int remainder = num1 % num2;
+
+  print("Sum: $sum");
+  print("Difference: $difference");
+  print("Product: $product");
+  print("Quotient: $quotient");
+  print("Remainder: $remainder");
+}
