c100c8fcb534a94f45ac83a8efd6428a215a4bb3
Jagrata Deb <jagratadeb2004@gmail.com>
2025-06-28 22:56:13 +0530

    add code for logical operator

---
+++ 05 Operators/02comparison.dart
@@ -0,0 +1,13 @@
+// Dart program to demonstrate comparison operators
+void main() {
+  int a = 10;
+  int b = 5;
+
+  print('a = $a, b = $b');
+  print('a == b: ${a == b}');      // Equal to
+  print('a != b: ${a != b}');      // Not equal to
+  print('a > b: ${a > b}');        // Greater than
+  print('a < b: ${a < b}');        // Less than
+  print('a >= b: ${a >= b}');      // Greater than or equal to
+  print('a <= b: ${a <= b}');      // Less than or equal to
+}
