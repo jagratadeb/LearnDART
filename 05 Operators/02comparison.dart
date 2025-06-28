99044d39054670be2e9f5c33e69eb0ef0429be93
Jagrata Deb <jagratadeb2004@gmail.com>
2025-06-28 23:02:11 +0530

    update the code for comparison operator

--- 05 Operators/02comparison.dart
+++ 05 Operators/02comparison.dart
@@ -2,12 +2,14 @@
 void main() {
   int a = 10;
   int b = 5;
+  int c = 10;
 
-  print('a = $a, b = $b');
-  print('a == b: ${a == b}');      // Equal to
-  print('a != b: ${a != b}');      // Not equal to
-  print('a > b: ${a > b}');        // Greater than
-  print('a < b: ${a < b}');        // Less than
-  print('a >= b: ${a >= b}');      // Greater than or equal to
-  print('a <= b: ${a <= b}');      // Less than or equal to
+  print('a = $a, b = $b, c = $c');
+  print('a == b: ${a == b}'); // Equal to
+  print('a == c: ${a == c}'); // Equal to
+  print('a != b: ${a != b}'); // Not equal to
+  print('a > b: ${a > b}'); // Greater than
+  print('b < a: ${b < a}'); // Less than
+  print('a >= c: ${a >= c}'); // Greater than or equal to
+  print('b <= a: ${b <= a}'); // Less than or equal to
 }
