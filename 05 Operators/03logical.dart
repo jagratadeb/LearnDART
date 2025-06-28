ea7a2c16b9c55fd08034f90c72d7d9ce3524dc2a
Jagrata Deb <jagratadeb2004@gmail.com>
2025-06-28 23:01:03 +0530

    improved code

--- 05 Operators/03logical.dart
+++ 05 Operators/03logical.dart
@@ -1,9 +1,19 @@
 // Dart program to demonstrate logical AND (&&) and OR (||) operators
+// ignore_for_file: dead_code
+
 void main() {
   bool a = true;
   bool b = false;
 
   print('a = $a, b = $b');
-  print('a && b (AND): ${a && b}'); // Logical AND
-  print('a || b (OR): ${a || b}'); // Logical OR
+
+  // Logical AND
+  print('a && b: ${a && b}');
+  print('a && true: ${a && true}');
+  print('b && true: ${b && true}');
+
+  // Logical OR
+  print('a || b: ${a || b}');
+  print('a || false: $a');
+  print('b || false: ${b || false}');
 }
