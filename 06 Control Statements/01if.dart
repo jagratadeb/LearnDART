edc3cbf90a718f3f40bb44ae688572299ce9e718
Jagrata Deb <jagratadeb2004@gmail.com>
2025-06-30 14:07:48 +0530

    add code for control statements (if and if-else)

---
+++ 06 Control Statements/01if.dart
@@ -0,0 +1,8 @@
+// Dart program to demonstrate if statement
+void main() {
+  int number = 10;
+
+  if (number > 0) {
+    print('$number is positive');
+  }
+}
---
+++ 06 Control Statements/02ifelse.dart
@@ -0,0 +1,10 @@
+// Dart program to demonstrate if-else statement
+void main() {
+  int number = -5;
+
+  if (number > 0) {
+    print('$number is positive');
+  } else {
+    print('$number is not positive');
+  }
+}
