19827f72e62538f56cafcdf21d08156e3b232e8d
Jagrata Deb <jagratadeb2004@gmail.com>
2025-07-01 16:45:39 +0530

    add code for string interpolation

---
+++ 07 Strings/02interpolation.dart
@@ -0,0 +1,11 @@
+// Example for string interpolation in Dart
+void main() { 
+  String name = "Jagrata Deb";
+  int age = 20;
+
+  // Using string interpolation to create a formatted string
+  String introduction = "My name is $name and I am $age years old.";
+  
+  // Printing the result
+  print(introduction); // Output: My name is Jagrata Deb and I am 25 years old.
+} 
