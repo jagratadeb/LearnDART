0d4887179710a01194ad61115c7fa46bc157b541
Jagrata Deb <jagratadeb2004@gmail.com>
2025-07-02 19:17:05 +0530

    add code for loops (for and while)

---
+++ 08 Loop/01for.dart
@@ -0,0 +1,7 @@
+// Dart program to demonstrate a for loop
+void main() {
+  // Print numbers from 1 to 5 using a for loop
+  for (int i = 1; i <= 5; i++) {
+    print('Number: $i');
+  }
+}
---
+++ 08 Loop/02while.dart
@@ -0,0 +1,9 @@
+void main() {
+  // Example: Print numbers from 1 to 5 using a while loop
+  int i = 1;
+  while (i <= 5) {
+    print(i);
+    i++;
+  }
+  print("Done!");
+}
